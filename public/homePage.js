43bf11aec295c273a96c823834348cd896c2a350
Kirill Shikov <rbhz2806@mail.ru>
2025-12-21 09:42:15 +0300

    итотговая работа

--- public/homePage.js
+++ public/homePage.js
@@ -32,68 +32,84 @@ fetchRates();
 setInterval(fetchRates, 60000);
 
 const moneyManager = new MoneyManager();
-const favoritesWidget = new FavoritesWidget();
-
-ApiConnector.getFavorites(function(response) {
-    if (response.success) {
-        favoritesWidget.clearTable();
-        favoritesWidget.fillTable(response.data);
-        moneyManager.updateUsersList(response.data);
-        console.log('Список избранных пользователей успешно загружен');
-    } else {
-        console.log('Ошибка при загрузке списка избранного');
-    }
-});
-
 moneyManager.addMoneyCallback = function(data) {
-    ApiConnector.addMoney(data, function(response) {
-        if (response.success) {
-            ProfileWidget.showProfile(response.data);
-            moneyManager.setMessage(true, 'Баланс успешно пополнен');
-            moneyManager.addMoneyForm.reset();
-        } else {
-            moneyManager.setMessage(false, response.error);
-        }
-    });
+	ApiConnector.addMoney(data, function(response) {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+			moneyManager.setMessage(
+				true,
+				'баланс успешно пополнен'
+			);
+			moneyManager.addMoneyForm.reset();
+		} else {
+			let errorMsg = response.error || response.message || 'ошибка при пополнении баланса';
+            moneyManager.setMessage(false, errorMsg);
+		}
+	});
 };
 
 moneyManager.conversionMoneyCallback = function(data) {
-    ApiConnector.convertMoney(data, function(response) {
-        if (response.success) {
-            ProfileWidget.showProfile(response.data);
-            moneyManager.setMessage(true, 'Конвертация успешно выполнена');
-            moneyManager.conversionMoneyForm.reset();
-        } else {
-            moneyManager.setMessage(false, response.error);
-        }
-    });
+	ApiConnector.convertMoney(data, function(response) {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+			moneyManager.setMessage(
+				true,
+				'конвертация успешно выполнена'
+			);
+			moneyManager.conversionMoneyForm.reset();
+		} else {
+			moneyManager.setMessage(
+				false,
+				response.message || 'ошибка при конвертации валюты'
+			);
+		}
+	});
 };
 
 moneyManager.sendMoneyCallback = function(data) {
-    ApiConnector.transferMoney(data, function(response) {
-        if (response.success) {
-            ProfileWidget.showProfile(response.data);
-            moneyManager.setMessage(true, 'Перевод успешно выполнен');
-            moneyManager.clearForm();
-        } else {
-            moneyManager.setMessage(false, response.error);
-        }
-    });
+	ApiConnector.transferMoney(data, function(response) {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+			moneyManager.setMessage(
+				true,
+				'перевод успешно выполнен'
+			);
+			moneyManager.sendMoneyForm.reset();
+		} else {
+			let errorMsg = response.error || response.message || 'ошибка при переводе средств';
+            moneyManager.setMessage(false, errorMsg);
+		}
+	});
 };
 
+const favoritesWidget = new FavoritesWidget();
+
+ApiConnector.getFavorites(function(response) {
+	if (response.success) {
+		favoritesWidget.clearTable();
+		favoritesWidget.fillTable(response.data);
+		moneyManager.updateUsersList(response.data);
+		console.log('список избранных пользователей успешно загружен');
+	} else {
+		console.log('ошибка при загрузке списка избранного');
+	}
+});
+
 favoritesWidget.addUserCallback = function(userData) {
-    ApiConnector.addUserToFavorites(userData, function(response) {
-        if (response.success) {
-            favoritesWidget.clearTable();
-            favoritesWidget.fillTable(response.data);
-            favoritesWidget.updateUsersList(response.data);
-            favoritesWidget.setMessage('Пользователь добавлен в избранное', 'success');
-            console.log('Пользователь добавлен в избранное');
-        } else {
-            favoritesWidget.setMessage(response.error, 'error');
-            console.log('Ошибка при добавлении пользователя:', response.error);
-        }
-    });
+	ApiConnector.addUserToFavorites(userData, function(response) {
+		if (response.success) {
+			favoritesWidget.clearTable();
+			favoritesWidget.fillTable(response.data || userData);
+			let updatedUsers = response.data || [userData];
+            moneyManager.updateUsersList(updatedUsers);
+			favoritesWidget.setMessage("Пользователь успешно добавлен в избранное", 'success');
+			console.log('Пользователь успешно добавлен в избранное');
+		} else {
+			 const errorMsg = response.error || "Не удалось добавить пользователя";
+            favoritesWidget.setMessage(false, `Ошибка: ${errorMsg}`);
+			console.log('Ошибка при добавлении пользователя:', response.error);
+		}
+	});
 };
 
 favoritesWidget.removeUserCallback = function(userData) {
@@ -101,11 +117,13 @@ favoritesWidget.removeUserCallback = function(userData) {
 		if (response.success) {
 			favoritesWidget.clearTable();
 			favoritesWidget.fillTable(response.data);
-			favoritesWidget.updateUsersList(response.data);
+			let updatedUsers = response.data || [];
+            moneyManager.updateUsersList(updatedUsers);
 			favoritesWidget.setMessage('пользователь успешно удален из избранного', 'success');
 			console.log('пользователь успешно удален из избранного');
 		} else {
-			favoritesWidget.setMessage(`Ошибка: ${response.error || "Не удалось удалить пользователя"}`, "error");
+			const errorMsg = response.error || "Не удалось удалить пользователя";
+            favoritesWidget.setMessage(false, `Ошибка: ${errorMsg}`);
 			console.log('ошибка при удаление пользователя', response.error);
 		}
 	});
